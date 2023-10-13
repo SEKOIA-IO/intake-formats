fcf17028fcfc0d2e42950b7651fa406622aed763
Pierrick VERAN <pierrick.veran@sekoia.fr>
2023-10-13 22:18:40 +0200

    feat(test_formats): add GitHub check for test_linting

--- utils/test_linting.py
+++ utils/test_linting.py
@@ -31,7 +31,10 @@ def test_linting_test_files(intakes_root, test_path):
     """Ensure that keys are sorted in tests files"""
     test_fullpath = os.path.join(intakes_root, test_path)
     with open(test_fullpath) as f:
-        new_test_file = format_test(json.load(f))
+        current_test_file = json.load(f)
 
+    new_test_file = format_test(current_test_file)
     with open(test_fullpath, "w") as out:
         json.dump(new_test_file, out, indent=2)
+
+    assert new_test_file == current_test_file
