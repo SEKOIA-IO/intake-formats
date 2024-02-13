5d4edecb5b8329c28c48fc84074bc9105ec6b4e8
lvoloshyn-sekoia <leonid.voloshyn@sekoia.io>
2024-02-13 12:23:31 +0200

    Improve smart descriptions tool

--- utils/show_smart_descriptions.py
+++ utils/show_smart_descriptions.py
@@ -74,8 +74,7 @@ class SmartDescriptionManager(IntakeTestManager):
 
             return "`%s`" % str(field_parsed_value)
 
-        message = re.sub(r"(\{[a-zA-Z\.\_]+\})", sub_fields, message)
-
+        message = re.sub(r"(\{[a-zA-Z0-9\.\_]+\})", sub_fields, message)
         return message
 
     def run(self, prsha: str | None = None):
